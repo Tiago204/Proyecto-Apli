a22933b025b96a7291baf108464d3a4999c96da6
Juerte99 <julian.jar.rivero@gmail.com>
2021-08-23 15:48:34 -0300

    prueba
    solo una pruebva de llamada

--- Bola 0.01/Assets/Scripts/Movimiento.cs
+++ Bola 0.01/Assets/Scripts/Movimiento.cs
@@ -23,10 +23,15 @@ public class Movimiento : MonoBehaviour
     {
         
     }
+    private void Update()
+    {
+        Debug.Log(rb.velocity.magnitude);
+        Moviendo();
+    }
     void FixedUpdate()
     {
         camDirection();
-        Moviendo();
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -40,18 +45,24 @@ public class Movimiento : MonoBehaviour
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
         movinput = new Vector3(h, 0, v);
-        movinput = Vector3.ClampMagnitude(movinput, 1);
+        movinput.Normalize();
 
-        movvector += movinput.x * camright + movinput.z * camforward;
-        movvector = Vector3.ClampMagnitude(movvector, MaxAcceleration);
+        movvector = movinput.x * camright + movinput.z * camforward;
+
+
+
+
+        //movvector = Vector3.ClampMagnitude(movvector, MaxAcceleration);
         rb.AddForce(movvector * fuerza * Time.deltaTime, ForceMode.Force);
-        
+        //rb.velocity += movvector * fuerza * Time.deltaTime;
+        //rb.velocity = Vector3.ClampMagnitude(rb.velocity, MaxAcceleration);
         if (Input.GetKey("space") && puedosaltar == true)
         {
             puedosaltar = false;
             rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
         }
-        Debug.Log(rb.velocity.magnitude);
+        
+        
     }
     void camDirection()
     {
@@ -61,7 +72,7 @@ public class Movimiento : MonoBehaviour
         camforward.y = 0;
         camright.y = 0;
 
-        camforward = camforward.normalized;
-        camright = camright.normalized;
+        //camforward = camforward.normalized;
+        //camright = camright.normalized;
     }
 }
